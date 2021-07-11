8f0e463ee548533d5906d86e555501c8bbb7c43b
Niall McGinness <njwmcginness@gmail.com>
2021-07-11 04:03:03 -0700

    filesystem path check whether dir exists

--- tests/fileSystemCheck.cpp
+++ tests/fileSystemCheck.cpp
@@ -27,10 +27,10 @@ int main(){
     fs::path project_root  = fs::current_path();
     fs::path data_dir = project_root / "data";
 
-    bool is_dir = fs::is_directory(data_dir);
+    auto is_dir = fs::is_directory(data_dir);
     std::cout << "project_root " << project_root << "\n"
               << " data_dir  " << data_dir << '\n'
-              << "root directory " << is_dir << '\n'
+              << "data dir found ?  " << is_dir << '\n'
               << "relative path " << project_root.relative_path() << '\n';
 
 }
