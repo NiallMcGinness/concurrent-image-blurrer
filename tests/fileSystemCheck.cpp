a47d53cb951b952dff52bb4e8b8b7a3c4f04c269
Niall McGinness <njwmcginness@gmail.com>
2021-07-11 03:40:01 -0700

    added filesystem tests

--- tests/fileSystemCheck.cpp
+++ tests/fileSystemCheck.cpp
@@ -11,7 +11,7 @@ int main(){
 
    std::string data_dir = ".";
 
-   auto data_dir_path = std::filesystem::path(data_dir);
+   std::filesystem::path data_dir_path = std::filesystem::path(data_dir);
 
    std::cout << "data directory relative path " << data_dir_path << std::endl;
 
