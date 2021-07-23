10591e1db04616d09b232279e848daafcee52dbc
Niall McGinness <njwmcginness@gmail.com>
2021-07-23 10:25:05 -0700

    added async test with out any file operations

--- tests/async_test.cpp
+++ tests/async_test.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 
-int print_thread(){
+int print_thread(int run_number){
 
-    int thread_id = this_thread::get_id();
-    cout << "future run on thread " << thread_id << '\n';
-    return thread_id;
+   // int thread_id = this_thread::get_id();
+    cout << "thread id" << thread_id << "run number " << run_number <<'\n';
+    return 0;
 }
 
 
@@ -20,6 +20,6 @@ int main(){
      std::vector<std::future<int> > tasks;
 
      for(int i = 0; i > number_of_threads; i++){
-         tasks.emplace_back(async(std::launch::async, print_thread()));
+         tasks.emplace_back(async(std::launch::async, print_thread(),i));
      }
 }
