d96272dc328681aca13de4a330e3cf91eb1a5c60
Niall McGinness <njwmcginness@gmail.com>
2021-08-07 21:20:59 -0700

    added single thread test

--- tests/single_thread.cpp
+++ tests/single_thread.cpp
@@ -9,6 +9,8 @@
 
 namespace fs = std::filesystem;
 
+void process_file_list(vector<string>,GetFiles);
+void load_and_process_image(string,string);
 
 int main(){
 
@@ -24,7 +26,6 @@ int main(){
         return 1;
     }
 
-     std::cout <<  project_root << "\n";
     /*
 
     get list of files in dir 
@@ -42,13 +43,47 @@ int main(){
     if (file_blocks.size() < 1 ) {
         return 1;
     }
-    
-    for ( auto &fb : file_blocks){
 
-        std::cout <<  "file_blocks : " << fb.at(0) << "\n";
+    /*
+
+    multithreaded  versionwill have one block of files per thread
+    in this case we're just testing one thread so the vector of fileblcoks shouldonly have one element 
+    */
+    
+   vector<string> file_list = file_blocks.at(0);
+   
+   process_file_list(file_list,gf);
+
+}
+
+
+void process_file_list(vector<string> file_list, GetFiles gf){
+
+    fs::path output_dir_path = gf.get_output_dir_path();
+
+    std::cout <<  "output_dir_path  : " << output_dir_path << "\n";
+    for ( auto &file_path : file_list){
+
+
+
+        std::cout <<  "file_path  : " << file_path << "\n";
+        fs::path file_name = fs::path(file_path).filename();
+        std::cout <<  "input file_name   : " << file_name  << "\n";
+
+        fs::path output_filepath = output_dir_path / file_name;
+
+        std::cout <<  "output file psth  : " << output_filepath  << "\n";
+        string output_filepath_string =  output_filepath.string();
+        std::cout <<  "output file string path   : " << output_filepath_string   << "\n";
+
+
 
 
     }
+}
+
+
+void load_and_process_image(string input_filepath, string output_filepath){
 
 }
 
@@ -70,5 +105,15 @@ std::cout << f << std::endl;
 
         std::cout << "Absolute path for " << p << " is " 
               << std::filesystem::absolute(p) << '\n';
+
+
+
+ 
+    for ( auto &fb : file_blocks){
+
+        std::cout <<  "file_blocks : " << fb.at(0) << "\n";
+
+
+    }
 
 */
