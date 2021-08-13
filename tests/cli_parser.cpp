70ec02f83485ede1bdaae7be41288bd9918c76a0
Niall McGinness <njwmcginness@gmail.com>
2021-08-12 20:35:30 -0700

    added short flags as well as long flags to cli parser

--- tests/cli_parser.cpp
+++ tests/cli_parser.cpp
@@ -1,4 +1,4 @@
-#include <cxxopts.hpp>
+#include "../src/cxxopts.h"
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -9,20 +9,20 @@ using namespace std;
 int main(int argc, char *argv[]) {
   try {
     cxxopts::Options options("concurrent image blurrer", "cli flag parser");
-    options.add_options()("threads", "number of threads to run program",
-                          cxxopts::value<uint>())(
-        "cycles", "number of cycles to run", cxxopts::value<uint>());
+    options.add_options()("t,threads", "number of threads to run program",
+                          cxxopts::value<int>())(
+        "c,cycles", "number of cycles to run", cxxopts::value<int>());
 
     const auto result = options.parse(argc, argv);
 
     if (result.count("threads")) {
-      uint threads_flag = result["threads"].as<uint>();
-      cout << "threads" << threads_flag << "\n";
+      int threads_flag = result["threads"].as<int>();
+      cout << "threads : " << threads_flag << "\n";
     }
 
     if (result.count("cycles")) {
-      uint cycles_flag = result["cycles"].as<uint>();
-      cout << "cycles " << cycles_flag << "\n";
+      int cycles_flag = result["cycles"].as<int>();
+      cout << "cycles : " << cycles_flag << "\n";
     }
   } catch (exception &e) {
     cerr << e.what();
